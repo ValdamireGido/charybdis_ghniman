830a9b47102e7642b367d777f2b424e1aaac7053
Volo <valdamire@gmail.com>
2026-01-13 23:40:34 +0100

    feat: added qmk compile for linux (shell)

---
+++ qmk_compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# qmk json2c ghniman_charybdis.json > keyboards/bastardkb/charybdis/4x6/keymaps/ghniman/keymap.c && {
+util/docker_cmd.sh qmk json2c -o keyboards/bastardkb/charybdis/4x6/keymaps/ghniman/keymap.c ghniman_charybdis.json && {
+    echo "=== compiled json keymap to C code"
+} || {
+    echo "!!! could not compile layout json to C"
+    exit 1
+}
+
+# qmk compile -kb bastardkb/charybdis/4x6/v2/splinky_3 -km ghniman && {
+util/docker_build.sh bastardkb/charybdis/4x6/v2/splinky_3:ghniman && {
+    echo "=== compiled firmware"
+} || {
+    echo "!!! could not compile firmware"
+    exit 2
+}
